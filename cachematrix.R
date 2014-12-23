513a1314f30c2738e07f02830d2a8ae8d21dc175
iripo <irinysin@gmail.com>
2014-12-23 14:10:03 +0400

    Revert "two functions"
    This reverts commit d37da09bfc3a9f1667cc32220633e26d4eef16db.

--- cachematrix.R
+++ cachematrix.R
@@ -1,50 +1,15 @@
-## makeCacheMatrix creates a special "matrix" 
+## Put comments here that give an overall description of what your
+## functions do
 
-makeCacheMatrix <- function(x = matrix()) { 
+## Write a short comment describing this function
 
-##  set the value of the matrix
-    	  inv <- NULL
-        set <- function(y) {
-                x <<- y
-                inv <<- NULL
-        }
-## get the value of the matrix
- 
-        get <- function() x
-
-## set the value of the iversed matrix
-
-        setInv <- function(solve) inv <<- solve
-
-## get the value of the inversed matrix
-
-        getInv <- function() inv
-        list(set = set, get = get,
-             setInv = setInv,
-             getInv = getInv)
+makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## CasheSolve computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-
-## sets inv equal to the inverse matrix
-
-        inv <- x$getInv()
-
-## Checked if inverse has already been calculated and retrieve the inverse from the cache.
-        if(!is.null(inv)) {
-                message("getting cached data")
-                return(inv)
-        }
-
-## If inverse has not been calculated then calculate it and print
-
-        data <- x$get()
-        inv <- solve(data, ...)
-        x$setInv(inv)
-        inv
+        ## Return a matrix that is the inverse of 'x'
 }
-
